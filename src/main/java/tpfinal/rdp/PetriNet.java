9290b42a37912a2e0b5f99ebcbd403ce3c688213
Nicolas Melia <nicolas.melia@mi.unc.edu.ar>
2026-01-20 22:13:16 -0300

    feat: new changes

--- src/main/java/tpfinal/rdp/PetriNet.java
+++ src/main/java/tpfinal/rdp/PetriNet.java
@@ -1,4 +1,5 @@
 package tpfinal.rdp;
+import tpfinal.utils.MathUtils;
 
 public class PetriNet {
 
@@ -41,48 +42,6 @@ public class PetriNet {
         this.transitions = INCIDENCE_MATRIX[0].length;
     }
 
-    public static int[][] multiplyMatrix(int[][] m1, int[][] m2) {
-        if (m1[0].length != m2.length) {
-            throw new IllegalArgumentException("The number of columns of the first matrix must be equal to the number of rows of the second matrix");
-        }
-
-        int filasA = m1.length;
-        int columnasA = m1[0].length;
-        int columnasB = m2[0].length;
-
-        int[][] result = new int[filasA][columnasB];
-
-        for (int i = 0; i < filasA; i++) {
-            for (int j = 0; j < columnasB; j++) {
-                result[i][j] = 0;
-                for (int k = 0; k < columnasA; k++) {
-                    result[i][j] += m1[i][k] * m2[k][j];
-                }
-            }
-        }
-
-        return result;
-    }
-
-    public static int[][] addMatrix(int[][] m1, int[][] m2) {
-        if (m1.length != m2.length || m1[0].length != m2[0].length) {
-            throw new IllegalArgumentException("Matrixs must have the same dimensions for addition.");
-        }
-
-        int filas = m1.length;
-        int columnas = m1[0].length;
-        int[][] result = new int[filas][columnas];
-
-        // Sumar elemento por elemento
-        for (int i = 0; i < filas; i++) {
-            for (int j = 0; j < columnas; j++) {
-                result[i][j] = m1[i][j] + m2[i][j];
-            }
-        }
-
-        return result;
-    }
-
     public static void imprimirMatriz(int[][] matriz) {
         System.out.print("[ "); 
         
@@ -107,7 +66,7 @@ public class PetriNet {
             }
         }
 
-        int[][] multiply = PetriNet.multiplyMatrix(PetriNet.INCIDENCE_MATRIX, fireMatrix);
+        int[][] multiply = MathUtils.multiplyMatrix(PetriNet.INCIDENCE_MATRIX, fireMatrix);
         return multiply;
     }
 
@@ -123,8 +82,8 @@ public class PetriNet {
                 }
             }
 
-        int[][] mult = PetriNet.multiplyMatrix(W, s);
-        int[][] result = PetriNet.addMatrix(m_i, mult);
+        int[][] mult = MathUtils.multiplyMatrix(W, s);
+        int[][] result = MathUtils.addMatrix(m_i, mult);
         currentMarking = result;
 
         return result;
